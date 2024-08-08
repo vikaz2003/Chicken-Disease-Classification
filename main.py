370a77135cd3913fdadc6726a9ae85123388cd72
vikaz2003 <vikaskunch03@gmail.com>
2024-08-08 08:35:44 +0530

    Prepare base model completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.ChickenDisease import logger
 from src.ChickenDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.ChickenDisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion Stage"
@@ -14,3 +14,15 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
